09483aeb52797c09c4d6507c1cc5c816ae598e6c
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-02-24 05:50:08 +0200

    Optimized NavBar (created SearchBar component)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import logo from "../assets/medium-logo.png";
 import { Bars3Icon } from '@heroicons/react/24/solid'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
     return (
@@ -8,13 +8,8 @@ const NavBar = () => {
         <div className="flex">
         <Bars3Icon className="size-4 cursor-pointer opacity-30 mt-3 ml-3" />
         <img src={logo} alt="alt" className="w-21 cursor-pointer m-3 mr-2"/>
-
-    <div className="relative w-40 ">
-            <MagnifyingGlassIcon className="absolute w-4 left-2 top-3 opacity-50"/>
-        <input type="text" placeholder="Search" className="bg-[#F9F9F9] w-full text-[10px] h-8 font-sans pl-8 pr-3 pt-0.7 rounded-full focus:outline-none mt-1 font-semibold "/>
+        <SearchBar/>
     </div>
-
-        </div>
             <button className="">To be continued...</button>
         <div>
 
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+const SearchBar = () => {
+    return (
+    <div className="relative w-40 ">
+        <MagnifyingGlassIcon className="absolute w-4 left-2 top-3 opacity-30"/>
+        <input type="text" placeholder="Search" className="bg-[#F9F9F9] w-full text-[10px] h-8 font-sans pl-8 pr-3 pt-0.7 rounded-full focus:outline-none mt-1 font-semibold "/>
+    </div>
+    )
+}
+
+export default SearchBar
