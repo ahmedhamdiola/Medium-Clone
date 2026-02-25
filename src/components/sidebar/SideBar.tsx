6abebbc998ece7d8aace532fe703bc5271d4b2fa
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-02-25 06:42:41 +0200

    cleaning code on sideBar components

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,23 +1,13 @@
-import { HomeIcon, BookmarkIcon, UserIcon, DocumentTextIcon, ChartBarIcon } from "@heroicons/react/24/outline"
-import { useState } from "react"
+
 import SideBarButton from "./SideBarButton"
 
-const options = [
-    {id: 1, name: 'Home', icon: HomeIcon},
-    {id: 2, name: 'Library', icon: BookmarkIcon}, 
-    {id: 3, name: 'Profile', icon: UserIcon},
-    {id: 4, name: 'Stories', icon: DocumentTextIcon},
-    {id: 5, name: 'Stats', icon: ChartBarIcon}]
+
 const SideBar = () => {
     return (
     <div className="pt-3">
         <div>
         {
-            options.map((option) => {
-                return(
-                    <SideBarButton name={option.name} icon={option.icon}/>
-                )
-            })
+            <SideBarButton />
         }
         </div>
     </div>
--- src/components/sidebar/SideBarButton.tsx
+++ src/components/sidebar/SideBarButton.tsx
@@ -1,28 +1,31 @@
-import { 
-    HomeIcon 
-    } from "@heroicons/react/24/outline"
 import { useEffect, useState } from "react"
-interface Props{
-    name: string,
-    icon: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
-    title?: string;
-    titleId?: string;
-} & React.RefAttributes<SVGSVGElement>>
-}
-const SideBarButton = (props : Props) => {
+import { HomeIcon, BookmarkIcon, UserIcon, DocumentTextIcon, ChartBarIcon } from "@heroicons/react/24/outline"
+
+const SideBarButton = () => {
+    const options = [
+    {id: 1, name: 'Home', icon: HomeIcon},
+    {id: 2, name: 'Library', icon: BookmarkIcon}, 
+    {id: 3, name: 'Profile', icon: UserIcon},
+    {id: 4, name: 'Stories', icon: DocumentTextIcon},
+    {id: 5, name: 'Stats', icon: ChartBarIcon}]
     const [active,setActive] = useState('home')
-    const Icon = props.icon
-    useEffect(() => {
-        console.log(active)
-    }, [active])
     return (
-        <div className="flex box-border cursor-pointer opacity-50 hover:opacity-80 hover:opacity-80 border-l-1 border-transparent active:border-gray-800 h-5 mb-2.5" onClick={() => setActive(props.name)}>
-        <Icon className="size-4 ml-3" />
-        <button className="pl-3 text-[11px] font-semibold cursor-pointer">
-            {props.name}
-        </button>
-    </div>
-    )
-}
+        <>
+        {options.map((option) => {
+            const Icon = option.icon
+            return(
+                <div className="flex box-border cursor-pointer opacity-50 hover:opacity-80 hover:opacity-80 border-l-1 border-transparent active:border-gray-800 h-5 mb-2.5" 
+                onClick={() => setActive(option.name)}>
+
+                    <Icon className="size-4 ml-3" />
+                    <button className="pl-3 text-[11px] font-semibold cursor-pointer">
+                        {option.name}
+                    </button>
+                </div>
+                )
+            })
+    }
+        </>
+    )}
 
 export default SideBarButton
